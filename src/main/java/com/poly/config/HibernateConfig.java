a3321dbcc73cad67bf36d6aa1b5b5f0242753880
NGUYENEM\T450s <nguyenvanem2k3@gmail.com>
2023-04-16 14:34:23 +0700

    First commit

---
+++ src/main/java/com/poly/config/HibernateConfig.java
@@ -0,0 +1,42 @@
+package com.poly.config;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+
+
+public class HibernateConfig {
+	 private static final SessionFactory FACTORY;
+
+	    static {
+	        Configuration conf = new Configuration();
+
+	        Properties properties = new Properties();
+	        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+	        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=FINALASS_FPOLYSHOP_FA22_SOF205__SOF2041");
+	        properties.put(Environment.USER, "sa");
+	        properties.put(Environment.PASS, "123456");
+	        properties.put(Environment.SHOW_SQL, "true");
+
+	        conf.setProperties(properties);
+//	        conf.addAnnotatedClass(Catecory.class);
+	        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+	                .applySettings(conf.getProperties()).build();
+	        FACTORY = conf.buildSessionFactory(registry);
+
+	    }
+
+	    public static SessionFactory getFACTORY() {
+	        return FACTORY;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(getFACTORY());
+	    }
+}
